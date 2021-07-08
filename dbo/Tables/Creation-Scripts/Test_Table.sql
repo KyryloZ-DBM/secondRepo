5f78ca941671767730629e5c6a2a3e4e58b7b1d2
KyryloZhdanov <a959d62ff7f273f9a959d>
2021-07-08 11:38:56 +0000

    message 4

--- dbo/Tables/Creation-Scripts/Test_Table.sql
+++ dbo/Tables/Creation-Scripts/Test_Table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[Test_Table] (
     [Column_pk] CHAR (10)    NOT NULL,
     [Column]    NUMERIC (18) NULL,
-    [Descr]     NCHAR (10)   NULL
+    [Descr]     NCHAR (10)   NULL,
+    [col1]      NCHAR (10)   NULL
 );
 
 
