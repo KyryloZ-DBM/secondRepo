d2e6cfd4db4bad5499e33449cb51ec293d737202
KyryloZ-DBM <82807333+KyryloZ-DBM@users.noreply.github.com>
2021-09-30 19:37:40 +0300

    Update Table_2.sql

--- dbo/Tables/Creation-Scripts/Table_2.sql
+++ dbo/Tables/Creation-Scripts/Table_2.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[Table_2] (
     [col1]       NCHAR (10) NULL,
     [col2remane] NCHAR (10) NULL,
-    [addcol3]    NCHAR (10) NULL
+    [addcol3]    NCHAR (10) NULL,
+    [addcol4]    NCHAR (10) NULL
 );
 
 
