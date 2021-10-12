a77139a31131f0a65173eac14cbaf0252da5c3d8
KyryloZ-DBM <email>
2021-10-12 11:56:27 +0000

    coll13

--- dbo/Tables/Creation-Scripts/Table_1.sql
+++ dbo/Tables/Creation-Scripts/Table_1.sql
@@ -1,8 +1,1 @@
-CREATE TABLE [dbo].[Table_1] (
-    [col1] NCHAR (10) NULL,
-    [col2] NCHAR (10) NULL
-);
-
-
-GO
-
+CREATE TABLE [dbo].[Table_1](	  [col1] NCHAR(10) COLLATE SQL_Latin1_General_CP1_CI_AS  NULL	, [col2] NCHAR(10) COLLATE SQL_Latin1_General_CP1_CI_AS  NULL)GO
