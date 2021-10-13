ea70b4216746fdef8716ffe635426190b56f4075
KyryloZ-DBM <email>
2021-10-13 09:53:27 +0000

    add table 7, 8

---
+++ dbo/Tables/Creation-Scripts/Table_8.sql
@@ -0,0 +1,42 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Table_8](
+	[kk] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[gg] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[f] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[ff] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[g] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[90-] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[j] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[kkk] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[k] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[gf] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[fd] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[s] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[a] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[798] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[yu] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[u] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[yui] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[o] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[ip] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[uuu] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[9t] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[r7u5] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[6u5y4] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[5u] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[4h] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[45b] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[65] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[6] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[57n] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[8] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[i7] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[9m8o] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[890] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[,p.-[] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+
+GO
